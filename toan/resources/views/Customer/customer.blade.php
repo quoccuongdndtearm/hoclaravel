7dd772b3fe7d4595973f8704a05200285abe0daa
POM <ngoductoan53@gmail.com>
2019-08-21 10:27:47 +0700

    ádasd

---
+++ toan/resources/views/Customer/customer.blade.php
@@ -0,0 +1,179 @@
+<div class='content container col-sm-10' style="background-color:#fafafa;float: right;">
+          <div style="text-align: center;"><h2>Danh mục khách hàng</h2></div>
+          <div class="bao_kh" style="margin: 30px 0 15px 0">
+              
+              <input style="display: inline-block;width: 23%" type="search" name="" id="input" class="form-control" value="" required="required" placeholder="Nhập tên hoặc mã KH để tìm kiếm" title="">
+              <select style="width: 18%; display: inline-block;" name="" id="input" class="form-control" required="required">
+                <option value="">- Loại máy -</option>
+                <option value="">Kangaroo</option>
+                <option value="">Karofi</option>
+                <option value="">Korihome</option>
+              </select>
+              <button type="button" class="btn btn-primary">Tìm kiếm</button>
+
+              <!-- Thêm KH -->
+              <a style="float: right;" class="btn btn-primary" data-toggle="modal" href='#modal-themkh'><i class="fa fa-plus"></i> Thêm khách hàng</a>
+              <div class="modal fade" id="modal-themkh">
+                <div class="modal-dialog"   >
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                      <h4 class="modal-title">Tạo mới khách hàng</h4>
+                    </div>
+                    <div class="modal-body" style="overflow: auto;">
+                      <div class="form-group" style="margin-bottom: 25px;">
+                                <label for="">Mã khách hàng:</label>
+                                <input style="width: 60%; float: right;" type="text" class="form-control" id="" placeholder="Nhập mã KH">
+                      </div>
+                      <div class="form-group" style="margin-bottom: 25px;">
+                                <label for="">Tên khách hàng:</label>
+                                <input style="width: 60%; float: right;" type="text" class="form-control" id="" placeholder="Nhập tên KH">
+                      </div>
+                      <div class="form-group" style="margin-bottom: 25px;">
+                                <label for="">Địa chỉ:</label>
+                                <input style="width: 60%; float: right;" type="text" class="form-control" id="" placeholder="Nhập địa chỉ">
+                      </div>
+                      <div class="form-group" style="margin-bottom: 25px;">
+                                <label for="">Loại máy:</label>
+                                <select style="width: 60%; float: right;" name="" id="input" class="form-control" required="required">
+                                  <option value="">- Loại máy -</option>
+                                  <option value="">Kangaroo</option>
+                                  <option value="">Karofi</option>
+                                  <option value="">Korihome</option>
+                                </select>
+                      </div>
+                      <div class="form-group" style="margin-bottom: 25px;">
+                                <label for="">Số điện thoại:</label>
+                                <input style="width: 60%; float: right;" type="text" class="form-control" id="" placeholder="Nhập sđt (bắt buộc)">
+                      </div>
+                      <div class="form-group" style="margin-bottom: 25px;">
+                                <label for="">Mô tả:</label>
+                                <textarea style="width: 60%; float: right;" name="" id="input" class="form-control" rows="3" required="required"></textarea>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Đóng</button>
+                      <button type="button" class="btn btn-primary">Lưu</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- end Thêm KH -->
+
+          </div>
+          <table class="table table_boder table-hover">
+            <thead>
+              <tr style="text-align: center;">
+                <th>Mã KH</th>
+                <th>Tên KH</th>
+                <th>Địa chỉ</th>
+                <th>Loại máy</th>
+                <th>Tổng tiền hàng(VNĐ)</th>
+                <th>Lần cuối mua hàng</th>
+                <th>Nợ(VNĐ)</th>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+              <tr>
+                <td>FFH1222</td>
+                <td>Nguyễn Văn A</td>
+                <td>Khu đô thị Linh Đàm</td>
+                <td>Kangaroo</td>
+                <td>15.000.000</td>
+                <td>18/09/2019 - <i>12:20</i></td>
+                <td>100.000</td>
+                <td><a href="">Sửa</a></td>
+                <td><a href="">Xóa</a></td>
+              </tr>
+            </tbody>
+          </table>
+          <div style="text-align: center;">
+              <ul class="pagination pagination-sm" style="margin: 0">
+                <li><a href="#">1</a></li>
+                <li><a href="#">2</a></li>
+                <li><a href="#">3</a></li>
+                <li><a href="#">4</a></li>
+                <li><a href="#">5</a></li>
+                <li><a href="#">&raquo;</a></li>
+              </ul>
+            </div>
+        </div>
---
+++ toan/resources/views/DonHang/Don.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('includes.header')
+
+<body id="new">
+@include('includes.sideleft')
+@include('Donhang.donhang')
+</body>
+</html>
